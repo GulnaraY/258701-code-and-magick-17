04fe1df9d270311337e3bf275579f023eeace0ea
GulnaraY <guga.ufa@gmail.com>
2019-06-09 17:18:52 +0300

    Создаем похожих магов

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var setupWindow = document.querySelector('.setup');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241,43,107)', 'rgb(146,100,161)', 'rgb(56,159,117)', 'rgb(215, 210,55)', 'rgb(0, 0, 0)'];
@@ -10,6 +12,7 @@ var SIMILAR_CHARACTERS_COUNT = 4;
 var showSetupWindow = function () {
   if (setupWindow.classList.contains('hidden')) {
     setupWindow.classList.remove('hidden');
+    setupWindow.querySelector('.setup-similar').classList.remove('hidden');
   }
 };
 
@@ -21,7 +24,7 @@ var createCharacter = function (characterName, characterSurname, characterCoatCo
   var character = {
     name: characterName + ' ' + characterSurname,
     coatColor: characterCoatColor,
-    eyesColors: characterEyesColor
+    eyesColor: characterEyesColor
   };
   return character;
 };
@@ -34,6 +37,22 @@ var createCharacters = function () {
   return characters;
 };
 
-showSetupWindow();
-createCharacters();
+var showSimilarWizards = function () {
+  var characters = createCharacters();
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    return wizardElement;
+  };
 
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < SIMILAR_CHARACTERS_COUNT; i++) {
+    fragment.appendChild(renderWizard(characters[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+showSimilarWizards();
+showSetupWindow();
