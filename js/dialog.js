1d70795361e4fdb64a33cc669b2799207d07df8b
GulnaraY <guga.ufa@gmail.com>
2019-06-20 14:33:04 +0300

    соответствие код-гайду

--- js/dialog.js
+++ js/dialog.js
@@ -22,7 +22,7 @@ dialogHandle.addEventListener('mousedown', function (evt) {
     startCoords = {
       x: evtMove.clientX,
       y: evtMove.clientY
-    }
+    };
 
     setupWindow.style.top = (setupWindow.offsetTop - diff.y) + 'px';
     setupWindow.style.left = (setupWindow.offsetLeft - diff.x) + 'px';
@@ -34,10 +34,10 @@ dialogHandle.addEventListener('mousedown', function (evt) {
     document.removeEventListener('mouseup', onMouseUp);
 
     if (dragged) {
-      var onClickPreventDefault = function (evt) {
-        evt.preventDefault();
+      var onClickPreventDefault = function (evtPrevent) {
+        evtPrevent.preventDefault();
         dialogHandle.removeEventListener('click', onClickPreventDefault);
-      }
+      };
       dialogHandle.addEventListener('click', onClickPreventDefault);
     }
   };
