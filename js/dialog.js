b3e9b338d47025e771ae8b112642f0dbf6114cb3
GulnaraY <guga.ufa@gmail.com>
2019-06-24 15:03:38 +0300

    разделение на модули

--- js/dialog.js
+++ js/dialog.js
@@ -1,48 +1,50 @@
 'use strict';
-var setupWindow = document.querySelector('.setup');
-var dialogHandle = document.querySelector('.upload');
 
-dialogHandle.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
+(function () {
+  var setupWindow = document.querySelector('.setup');
+  var dialogHandle = document.querySelector('.upload');
 
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-  var dragged = false;
-
-  var onMouseMove = function (evtMove) {
-    dragged = true;
-    var diff = {
-      x: startCoords.x - evtMove.clientX,
-      y: startCoords.y - evtMove.clientY
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    startCoords = {
-      x: evtMove.clientX,
-      y: evtMove.clientY
-    };
+    var dragged = false;
 
-    setupWindow.style.top = (setupWindow.offsetTop - diff.y) + 'px';
-    setupWindow.style.left = (setupWindow.offsetLeft - diff.x) + 'px';
-  };
-
-  var onMouseUp = function (evtUp) {
-    evtUp.preventDefault();
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-
-    if (dragged) {
-      var onClickPreventDefault = function (evtPrevent) {
-        evtPrevent.preventDefault();
-        dialogHandle.removeEventListener('click', onClickPreventDefault);
+    var onMouseMove = function (evtMove) {
+      dragged = true;
+      var diff = {
+        x: startCoords.x - evtMove.clientX,
+        y: startCoords.y - evtMove.clientY
       };
-      dialogHandle.addEventListener('click', onClickPreventDefault);
-    }
-  };
 
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
+      startCoords = {
+        x: evtMove.clientX,
+        y: evtMove.clientY
+      };
 
+      setupWindow.style.top = (setupWindow.offsetTop - diff.y) + 'px';
+      setupWindow.style.left = (setupWindow.offsetLeft - diff.x) + 'px';
+    };
+
+    var onMouseUp = function (evtUp) {
+      evtUp.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (evtPrevent) {
+          evtPrevent.preventDefault();
+          dialogHandle.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandle.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
